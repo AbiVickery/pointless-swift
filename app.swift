983fa77039f8d76eefb6253b8edf7f3b64522d1c
Abi Vickery <vickeryabi@gmail.com>
2020-01-06 11:53:13 -0600

    //

--- app.swift
+++ app.swift
@@ -37,50 +37,52 @@
 // moveForward()
 // collectGem()
 
-moveForward(
-    steps: 1;
-)
-turnLeft()
-moveForward(
-    steps: 3;
-)
-function turnRight(
-    turnLeft()
-    turnLeft()
-    turnLeft()
-)
-turnRight()
-turnLeft(
-    degrees: 0;
-)
-turnRight(
-    degrees: 180;
-)
-moveForward(
-    steps: 5;
-)
-collectGem(
-    color: red;
-)
-toggleSwitch(
-    action: jump;
-)
-jumpDown(
-    steps: 1;
-)
-speakOut(
-    dialouge: default;
-)
-jumpUp(
-    steps: 1;
-)
-pickUp(
-    grab: object;
-)
-thInk(
-    say: message;
-)
-register(
-    monster: boogeyman;
-)
+// moveForward(
+//     steps: 1;
+// )
+// turnLeft()
+// moveForward(
+//     steps: 3;
+// )
+// function turnRight(
+//     turnLeft()
+//     turnLeft()
+//     turnLeft()
+// )
+// turnRight()
+// turnLeft(
+//     degrees: 0;
+// )
+// turnRight(
+//     degrees: 180;
+// )
+// moveForward(
+//     steps: 5;
+// )
+// collectGem(
+//     color: red;
+// )
+// toggleSwitch(
+//     action: jump;
+// )
+// jumpDown(
+//     steps: 1;
+// )
+// speakOut(
+//     dialouge: default;
+// )
+// jumpUp(
+//     steps: 1;
+// )
+// pickUp(
+//     grab: object;
+// )
+// thInk(
+//     say: message;
+// )
+// register(
+//     monster: boogeyman;
+// )
 // Ive started to notice that some of the elements of swift are a lot like that of JavaScript...
+
+// Hello, World
